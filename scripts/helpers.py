1014f3f544a33e33969e22d7098810a6949e2920
José Videla Olmos <jvidelaolmos@gmail.com>
2023-10-24 11:12:35 -0300

    update

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -40,6 +40,7 @@ def format_command(logger, command, iac_tool, is_reviewed, review_required, revi
     paths_need_review = set1.intersection(set3)
     command_intersections = set1.intersection(set2)
 
+    logger.debug(f"command: {command}")
     logger.debug(f"words set: {set1}")
     logger.debug(f"paths that need review set : {set3}")
     logger.debug(f"Path that need review: {review_paths}")
